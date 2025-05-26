45ad40d18a8a7f7f6eda8c58bae4653f60718925
jpheneger <jpheneger@gmail.com>
2025-05-25 22:51:18 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from graphics import Window
+from maze import Maze
+import sys
+import random
+
+def main():
+    num_rows = 48
+    num_cols = 64
+    margin = 50
+    screen_x = 1024
+    screen_y = 768
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, random.randint(1, 100))
+    print("maze created")
+    is_solvable = maze.solve() 
+    if not is_solvable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
+    win.wait_for_close()
+
+
+main()
